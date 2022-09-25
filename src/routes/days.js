0fec64584851e15a96847ef1e357d3f80bf2449b
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2022-09-24 21:31:02 -0700

    edit the create. have to fix edit and name

--- src/routes/days.js
+++ src/routes/days.js
@@ -7,11 +7,11 @@ module.exports = db => {
       SELECT
         days.id,
         days.name,
-        COUNT(day_id) as spots
+        count(day_id) as spots
       FROM days
       JOIN timeslots ON timeslots.day_id = days.id
       GROUP BY days.id
-      ORDER BY days.id
+      ORDER BY days.id 
     `
     ).then(({ rows: days }) => {
       response.json(days);
@@ -20,3 +20,6 @@ module.exports = db => {
 
   return router;
 };
+
+
+//select count(events.day_id) as spots from days left join events on events.day_id = days.id group by days.id;
